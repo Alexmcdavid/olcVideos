538867bd3724c429473cebbe482526f762642737
Alex McDavid <alexisz@gmail.com>
2022-01-19 17:15:20 -0500

    main screen turn on

--- textris.cpp
+++ textris.cpp
@@ -46,6 +46,45 @@ int main(){
     z.append(".X..");
     z.append("....");
 
+    //initialize play field, may be able to draw this just once since ncurses refreshes only changes
+    int nFieldWidth {12};
+    int nFieldHeight {18};
+    std::vector<unsigned char> vField(nFieldWidth * nFieldHeight, 0); //swap for wide chars for better characters later, or ints with associations
+    for(int iter {}; iter < nFieldWidth * nFieldHeight; iter++){
+        if((iter % nFieldWidth == 0) || (iter % nFieldWidth == nFieldWidth - 1) || (iter >= nFieldWidth * nFieldHeight - nFieldWidth)) vField[iter] = 9;
+    }
+
+    //Initialize screen
+    int rows {};
+    int columns {};
+    initscr();
+    getmaxyx(stdscr, rows, columns);
+    noecho();
+    raw();
+
+    //initialize variables
+    bool bGameOver {false};
+
+    //Game loop
+    while(!bGameOver){
+
+        //draw field to stdscr
+        for(int x {}; x < nFieldWidth; x++){
+            for(int y {}; y < nFieldHeight; y++){
+                //replace mvprintw() with printw() and some looping later for efficiency
+                mvprintw(y, x, "%c", " ABCDEFG=#"[vField[y * nFieldWidth + x]]);
+                
+
+            }
+        }
+
+
+        getch(); //with implied refresh(), actually this will need to be made asynch so blocks can fall without waiting for input? or does Raw do this?
+        bGameOver = true; //break loop after once for now
+    }
+
+    getch();
+    endwin();
 
 
 
